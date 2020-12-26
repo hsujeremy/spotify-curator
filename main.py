3f3268def2a12c487a4e5a0316b3ce769fee2b27
Steve Li <lithafnium2000@gmail.com>
2020-12-26 15:27:35 -0500

    random songs

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 import os
+import random
 import spotipy
 import time
 import numpy as np
@@ -9,6 +10,27 @@ from spotipy.oauth2 import SpotifyClientCredentials
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import GridSearchCV
 
+def getRandomSongs(audio_f):
+    audio_features = []
+
+    for i in range(50):
+        offset = random.randint(0, 2000)
+        query = getRandomSearch()
+        songs = sp.search(query, limit=10, offset=offset)
+        if(songs):
+            for track in songs["tracks"]["items"]:
+                id = track['id']
+                features = sp.audio_features(id)
+                removed = remove_features(audio_f, features[0])
+                audio_features.append(removed)
+    
+    return audio_features
+
+def getRandomSearch():
+    characters = 'abcdefghijklmnopqrstuvwxyz'
+    index = random.randint(0, 25)
+
+    return characters[index] + '%'
 
 def remove_features(features, item):
   ret = {}
@@ -49,31 +71,31 @@ def setup():
     return spotipy.Spotify(client_credentials_manager=cc_manager)
 
 
-def add_album_songs(uri):
-    spotify_albums[uri] = {}
-    for attribute in song_attributes[:5]:
-        spotify_albums[uri][attribute] = []
+# def add_album_songs(uri):
+#     spotify_albums[uri] = {}
+#     for attribute in song_attributes[:5]:
+#         spotify_albums[uri][attribute] = []
 
-    tracks = sp.album_tracks(uri)
-    for item in tracks['items']:
-        spotify_albums[uri]['album'].append(album_names[album_count])
-        for attribute in song_attributes[1:5]:
-            spotify_albums[uri][attribute].append(item[attribute])
+#     tracks = sp.album_tracks(uri)
+#     for item in tracks['items']:
+#         spotify_albums[uri]['album'].append(album_names[album_count])
+#         for attribute in song_attributes[1:5]:
+#             spotify_albums[uri][attribute].append(item[attribute])
 
 
-def get_audio_features(album):
-    for attribute in song_attributes[5:]:
-        spotify_albums[album][attribute] = []
+# def get_audio_features(album):
+#     for attribute in song_attributes[5:]:
+#         spotify_albums[album][attribute] = []
 
-    count = 0
-    for track in spotify_albums[album]['uri']:
-        features = sp.audio_features(track)
-        for attribute in song_attributes[5:-1]:
-            spotify_albums[album][attribute].append(features[0][attribute])
+#     count = 0
+#     for track in spotify_albums[album]['uri']:
+#         features = sp.audio_features(track)
+#         for attribute in song_attributes[5:-1]:
+#             spotify_albums[album][attribute].append(features[0][attribute])
 
-        pop = sp.track(track)
-        spotify_albums[album]['popularity'].append(pop['popularity'])
-        count += 1
+#         pop = sp.track(track)
+#         spotify_albums[album]['popularity'].append(pop['popularity'])
+#         count += 1
 
 
 if __name__ == '__main__':
@@ -89,47 +111,59 @@ if __name__ == '__main__':
                        'valence', 'popularity')
     
     features = ['acousticness', 'danceability', 'energy', 'instrumentalness', 'liveness', 'loudness', 'speechiness', 'tempo', 'valence']
-    album_names = []
-    album_uris = []
-    for item in sp_albums['items']:
-        album_names.append(item['name'])
-        album_uris.append(item['uri'])
+    # album_names = []
+    # album_uris = []
+    # for item in sp_albums['items']:
+    #     album_names.append(item['name'])
+    #     album_uris.append(item['uri'])
 
-    spotify_albums = {}
+    # spotify_albums = {}
 
-    album_count = 0
-    for i in album_uris:
-        add_album_songs(i)
-        print('Songs from {} have been added to spotify_albums dictionary'.format(album_names[album_count]))
-        album_count += 1
+    # album_count = 0
+    # for i in album_uris:
+    #     add_album_songs(i)
+    #     print('Songs from {} have been added to spotify_albums dictionary'.format(album_names[album_count]))
+    #     album_count += 1
 
-    sleep_min = 2
-    sleep_max = 5
-    start_time = time.time()
-    request_count = 0
-    for i in spotify_albums:
-        get_audio_features(i)
-        request_count += 1
-        if request_count % 5 == 0:
-            print('{} playlists completed'.format(request_count))
-            time.sleep(np.random.uniform(sleep_min, sleep_max))
-            print('Loop #: {}'.format(request_count))
-            print('Elapsed Time: {} seconds'.format(time.time() - start_time))
+    # sleep_min = 2
+    # sleep_max = 5
+    # start_time = time.time()
+    # request_count = 0
+    # for i in spotify_albums:
+    #     get_audio_features(i)
+    #     request_count += 1
+    #     if request_count % 5 == 0:
+    #         print('{} playlists completed'.format(request_count))
+    #         time.sleep(np.random.uniform(sleep_min, sleep_max))
+    #         print('Loop #: {}'.format(request_count))
+    #         print('Elapsed Time: {} seconds'.format(time.time() - start_time))
 
-    dict_df = {}
-    for attribute in song_attributes:
-        dict_df[attribute] = []
+    # dict_df = {}
+    # for attribute in song_attributes:
+    #     dict_df[attribute] = []
 
-    for album in spotify_albums:
-        for feature in spotify_albums[album]:
-            dict_df[feature].extend(spotify_albums[album][feature])
+    # for album in spotify_albums:
+    #     for feature in spotify_albums[album]:
+    #         dict_df[feature].extend(spotify_albums[album][feature])
 
-    df = pd.DataFrame.from_dict(dict_df)
+    # df = pd.DataFrame.from_dict(dict_df)
 
-    df_2 = pd.DataFrame(get_songs_from_user(features, '12176195123'))
-    n_rows = len(df_2.index)
-    df_2['Label'] = np.ones(n_rows)
-    print(df_2)
+    df_user = pd.DataFrame(get_songs_from_user(features, '12176195123'))
+    n_user_songs = len(df_user.index)
+    user_labels = np.ones(n_user_songs)
+
+    df_random = pd.DataFrame(getRandomSongs(features))
+    n_random_songs = len(df_random.index)
+    random_labels = np.zeros(n_random_songs)
+    labels = np.append(user_labels, random_labels)
+    # print(labels)
+
+
+    # print(df_random)
+    # print(df_user)
+    # print(getRandomSearch())
+
+    data = pd.DataFrame.append(df_user, df_random)
     forest = RandomForestClassifier()
     forest_params = {
         "n_estimators": np.arange(10, 200, 10),
@@ -138,16 +172,16 @@ if __name__ == '__main__':
     }
     clf = GridSearchCV(forest, forest_params)
 
-    clf.fit(df_2, np.ones(n_rows))
+    clf.fit(data, labels)
     print(clf.best_params_)
 
-    print(df_2)
 
     # print('Original dataframe size: {}'.format(len(df)))
     # final_df = df.sort_values('popularity', ascending=False).drop_duplicates('name').sort_index()
     # print('Final dataframe size: {}'.format(len(final_df)))
 
-    df_2.to_csv('./user_liked_songs.csv')
+    df_user.to_csv('./user_liked_songs.csv')
+    df_random.to_csv('./random_songs.csv')
     # final_df.to_csv('./output.csv')
 
 
