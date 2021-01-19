af94362415cedec4f883e0052eb4e41862695610
Jeremy Hsu <jeremyhsu1@gmail.com>
2021-01-18 20:14:48 -0500

    Remove input/output Schema validation for now

--- server/spotify_model/ml_model_abc.py
+++ server/spotify_model/ml_model_abc.py
@@ -5,15 +5,15 @@ from abc import abstractmethod
 
 class MLModel(ABC):
     """Abstract base class for ML model prediction code. Original code by Brian Schmidt"""
-    @property
-    @abstractmethod
-    def input_schema(self):
-        raise NotImplementedError()
+    # @property
+    # @abstractmethod
+    # def input_schema(self):
+    #     raise NotImplementedError()
 
-    @property
-    @abstractmethod
-    def output_schema(self):
-        raise NotImplementedError()
+    # @property
+    # @abstractmethod
+    # def output_schema(self):
+    #     raise NotImplementedError()
 
     @abstractmethod
     def __init__(self):
--- server/spotify_model/spotify_predict.py
+++ server/spotify_model/spotify_predict.py
@@ -7,22 +7,23 @@ from ml_model_abc import MLModel
 
 
 class SpotifyModel(MLModel):
-    input_schema = Schema([{
-        'acousticness': float,
-        'danceability': float,
-        'energy': float,
-        'instrumentalness': float,
-        'liveness': float,
-        'loudness': float,
-        'speechiness': float,
-        'tempo': float,
-        'valence': float
-    }])
+    # # Need to find a way to validate ints too or convert ints to floats
+    # input_schema = Schema([{
+    #     'acousticness': float,
+    #     'danceability': float,
+    #     'energy': float,
+    #     'instrumentalness': float,
+    #     'liveness': float,
+    #     'loudness': float,
+    #     'speechiness': float,
+    #     'tempo': float,
+    #     'valence': float
+    # }])
 
-    output_schema = Schema({
-        'prediction': float,
-        'kmeans_prediction': float
-    })
+    # output_schema = Schema({
+    #     'prediction': float,
+    #     'kmeans_prediction': float
+    # })
 
     def __init__(self):
         dirpath = os.path.dirname(os.path.realpath(__file__))
@@ -31,13 +32,13 @@ class SpotifyModel(MLModel):
         self.kmeans_clf = joblib.load(os.path.join(dirpath, 'model_files', 'spotify_kmeans.joblib'))
 
     def predict(self, data):
-        # Call super method to validate data against input_schema
-        super().predict(data=data)
+        # # Call super method to validate data against input_schema
+        # super().predict(data=data)
 
         # Assume data is in the form of audio_features array in original file
         df_song = pd.DataFrame(data)
         prediction = self.clf.predict_proba(df_song)
         kmeans_prediction = self.kmeans_clf.predict(df_song)
         result = {'prediction': float(prediction[0][1]), 'kmeans_prediction': float(kmeans_prediction[0])}
-        self.output_schema.validate(result)
+        # self.output_schema.validate(result)
         return result
